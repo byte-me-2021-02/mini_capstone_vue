bcff92c0e82905a77a8d913704208638ad79a741
brian <riceb53@gmail.com>
2021-04-07 22:26:02 -0400

    add edit/update, still need to fix supplier_id frontend

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import ProductsShow from '../views/ProductsShow.vue'
+import ProductsEdit from '../views/ProductsEdit.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/products/:id',
     name: 'ProductsShow',
     component: ProductsShow
+  },
+  {
+    path: '/products/:id/edit',
+    name: 'ProductsEdit',
+    component: ProductsEdit
   },
   {
     path: '/about',
---
+++ src/views/ProductsEdit.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="products-edit">
+    <h1>{{ message }}</h1>
+    <form v-on:submit.prevent="submit">
+      
+      <p>name: <input type="text" v-model="product.name"></p>
+      <p>price: <input type="text" v-model="product.price"></p>
+      <p>description: <input type="text" v-model="product.description"></p>      
+      <p>quantity: <input type="text" v-model="product.quantity"></p>
+      <p>supplier_id: <input type="text" v-model="product.supplier_id"></p>
+      <input type="submit" value="Edit the product">
+    </form>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+
+// make a form
+// prepopulate the form
+  // getting data from the api based on the id in th url (show)
+  // add that data to the form
+// when the user submits the form, send form data to rails
+// redirect to the show page
+
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js! edit",
+      product: {}
+    };
+  },
+  created: function() {
+    axios.get(`/api/products/${this.$route.params.id}`).then(response => {
+      console.log(response.data)
+      this.product = response.data;
+    })
+  },
+  methods: {
+    submit: function() {
+      console.log('form was submitted')
+      // collect the data from the form
+      var params = {
+        name: this.product.name,
+        price: this.product.price,        
+        description: this.product.description,
+        supplier_id: this.product.supplier_id,
+        quantity: this.product.quantity,
+      }
+
+      // send that data to rails to the update action
+      axios.patch(`/api/products/${this.$route.params.id}`, params).then(response => {
+        console.log(response.data);
+        this.$router.push(`/products/${this.$route.params.id}`)
+      })
+    }
+  }
+};
+</script>
+
